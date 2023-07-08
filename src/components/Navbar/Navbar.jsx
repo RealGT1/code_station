16511dfe5ffa4589fe97f3354c047682c13fa6ce
Sourabh <bhatsourabh3@gmail.com>
2023-07-08 22:13:18 +0530

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
-import BedtimeTwoToneIcon from '@mui/icons-material/BedtimeTwoTone';
+import BedtimeTwoToneIcon from "@mui/icons-material/BedtimeTwoTone";
+
+
 const Navbar = () => {
+  const [showMessage, setShowMessage] = useState(false);
+
+  const handleClick = () => {
+    setShowMessage(true);
+  };
   return (
     <div className="title">
       Code
       <span className="t2">Station</span>
       <button className="Button1">Sign In</button>
       <div className="icons">
-      <BedtimeTwoToneIcon className="nightmode"/>
+        <BedtimeTwoToneIcon />
+      </div>
     </div>
-    </div>
-    
   );
 };
 
