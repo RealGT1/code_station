b2e0ac8d6e92c88a4c4eabfb7c6046c0048a320a
Sourabh <bhatsourabh3@gmail.com>
2023-07-08 20:39:41 +0530

    nightmode icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./navbar.css";
+import BedtimeTwoToneIcon from '@mui/icons-material/BedtimeTwoTone';
 const Navbar = () => {
-  return(
-   <div className="title">
-    Code 
-    <span className="t2">Station</span>
-   </div>
-  )
+  return (
+    <div className="title">
+      Code
+      <span className="t2">Station</span>
+      <button className="Button1">Sign In</button>
+      <div className="icons">
+      <BedtimeTwoToneIcon className="nightmode"/>
+    </div>
+    </div>
+    
+  );
 };
 
 export default Navbar;
