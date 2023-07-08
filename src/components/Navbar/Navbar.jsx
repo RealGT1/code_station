e31d13b27667e70232f8cf956e5b4ea9fe81f9f4
Sourabh <bhatsourabh3@gmail.com>
2023-07-08 22:32:20 +0530

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import "./navbar.css";
 import BedtimeTwoToneIcon from "@mui/icons-material/BedtimeTwoTone";
-
+import { Tooltip } from "@mui/material";
 
 const Navbar = () => {
-  const [showMessage, setShowMessage] = useState(false);
-
-  const handleClick = () => {
-    setShowMessage(true);
-  };
+  
   return (
     <div className="title">
       Code
       <span className="t2">Station</span>
       <button className="Button1">Sign In</button>
       <div className="icons">
+      <Tooltip title="You kidding me?!" followCursor>
         <BedtimeTwoToneIcon />
+        </Tooltip>
       </div>
     </div>
   );
