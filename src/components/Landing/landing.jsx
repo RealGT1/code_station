5e07fa99ed7cc125bb45b8f01cb9f267170b8466
SHREYAS <samarthpuru3557@gmail.com>
2023-07-09 23:29:13 +0530

    landing final update

--- src/components/Landing/landing.jsx
+++ src/components/Landing/landing.jsx
@@ -1,9 +1,11 @@
-<<<<<<< HEAD
-import React, { useEffect, useState } from "react";
-import "./landing.css";
-import Navbar from "../Navbar/Navbar";
-import teamimg from "./team.png"
-import collab from "./collab.png"
+import React, { useEffect, useState } from 'react';
+import './landing.css';
+import Navbar from '../Navbar/Navbar';
+import teamimg from './team.png';
+import collab from './collab.png';
+import { Link } from 'react-router-dom';
+import { animateScroll as scroll } from 'react-scroll';
+
 const Landing = () => {
   const [showButton, setShowButton] = useState(false);
 
@@ -20,34 +22,12 @@ const Landing = () => {
       }
     };
 
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener('scroll', handleScroll);
     return () => {
-      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener('scroll', handleScroll);
     };
   }, []);
-  return (
-    <div>
-      <Navbar />
 
-      <div className="text-main">
-        <p className="text a">Code.</p>
-        <p className="text b">Coffee.</p>
-        <p className="text c">Connect.</p>
-        <button className="myButton" >Get Started</button>
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/2092/2092494.png"
-          alt="img of codeeditor"
-          className="codeeditorimg"
-        />
-        <img src={collab} alt="team" className="collab" />
-=======
-import React, { useEffect } from 'react';
-import './landing.css';
-import Navbar from '../Navbar/Navbar';
-import { Link } from 'react-router-dom';
-import { animateScroll as scroll } from 'react-scroll';
-
-const Landing = () => {
   useEffect(() => {
     scroll.scrollToBottom({
       duration: 6000,
@@ -59,6 +39,7 @@ const Landing = () => {
   return (
     <div>
       <Navbar />
+
       <div className='text-main'>
         <p className='text a'>Code.</p>
         <p className='text b'>Coffee.</p>
@@ -66,7 +47,12 @@ const Landing = () => {
         <Link to='/start' className='link-button'>
           <button className='myButton'>Get Started</button>
         </Link>
->>>>>>> strt
+        <img
+          src='https://cdn-icons-png.flaticon.com/512/2092/2092494.png'
+          alt='img of codeeditor'
+          className='codeeditorimg'
+        />
+        <img src={collab} alt='team' className='collab' />
       </div>
     </div>
   );
