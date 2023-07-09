5925011b69d517ff71ffa8d1caaeac4dcf35779b
SHREYAS <samarthpuru3557@gmail.com>
2023-07-09 22:30:15 +0530

    with landing auto-scroll

--- src/components/Landing/landing.jsx
+++ src/components/Landing/landing.jsx
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './landing.css';
 import Navbar from '../Navbar/Navbar';
 import { Link } from 'react-router-dom';
+import { animateScroll as scroll } from 'react-scroll';
 
 const Landing = () => {
+  useEffect(() => {
+    scroll.scrollToBottom({
+      duration: 5000,
+      delay: 0,
+      smooth: 'easeInOutQuart',
+    });
+  }, []);
+
   return (
-    <div><Navbar />
+    <div>
+      <Navbar />
       <div className='text-main'>
         <p className='text a'>Code.</p>
         <p className='text b'>Coffee.</p>
         <p className='text c'>Connect.</p>
-        <Link to="/start" className='link-button'>
-          <button className='myButton' >Get Started</button>
+        <Link to='/start' className='link-button'>
+          <button className='myButton'>Get Started</button>
         </Link>
       </div>
     </div>
   );
-}
+};
 
 export default Landing;
